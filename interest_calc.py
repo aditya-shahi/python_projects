5e1ab0dc711792ea73e5ee6ef867c4f8ebd589e6
aditya-shahi <luckyadit.shahi@gmail.com>
2023-03-27 00:15:25 +0530

    Create interest_calc.py
    python program for calculating compound interest if, Amount, Time period and Interest rate is givn by the user.

---
+++ interest_calc.py
@@ -0,0 +1,29 @@
+# Accept the inputs
+
+startbalance = float(input("Enter the investment amount: "))
+years = int(input("Enter the number of years: "))
+rate = float(input("Enter the percentage interest rate: "))
+
+rate = rate/100
+
+total_interest = 0.0
+
+# Display the header for the table
+
+print("%4s%18s%10s%16s" % \
+	("Year", "Starting Balance", "Interest", "Ending Balance"))
+
+# Compute and display the results for each year
+
+for year in range (1, years+1):
+	interest = startbalance*rate
+	endbalance = startbalance + interest
+	print("%4d%18.2f%10.2f%16.2f" % \
+		(year, startbalance, interest, endbalance))
+
+	startbalance = endbalance
+	total_interest += interest  
+
+# Display the totals for the period
+print("Ending balance: $%0.2f" % endbalance)
+print("Total interest earned: $%0.2f" % total_interest)
